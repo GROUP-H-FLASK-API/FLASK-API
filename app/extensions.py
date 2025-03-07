519c217dc7c7805644453f5b10e8dd99880e403c
ayomi-20 <peaceayomirwoth@gmail.com>
2025-03-07 15:08:08 +0300

    group work

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
 
-db = SQLAlchemy()
-migrate = Migrate()
-bcrypt = Bcrypt()
-jwt = JWTManager()
+
+migrate = Migrate() #migrate instance
+db = SQLAlchemy()  #db instance
+bcrypt = Bcrypt() #new object of the Bcrypt() class
